ac764f08b44bf63e81e07fa9e8ab8080fc481554
Adam Epling <aepling@mindspace.net>
2015-07-18 03:01:33 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var address, env, port;
 env = 'dev';
 
 if (env === 'prod' || env === 'production') {
-  address = 'http://snappy-foos.herokuapp.com';
+  address = 'http://some-place-tbd.com';
   port = 80;
 } else {
   address = 'http://localhost';
@@ -13,9 +13,9 @@ module.exports = {
   ENVIRONMENT: env,
   PORT: port,
   API_URL: '/api/',
-  LOCAL_STORAGE_PREFIX: 'epzilla',
+  LOCAL_STORAGE_PREFIX: 'charlatan-abbey',
   ADDRESS: address,
   DEV_DB_ADDRESS: 'mongodb://localhost:27017/charlatan',
-  PROD_DB_ADDRESS: 'mongodb://snappycat:synapsefoosball@ds061661.mongolab.com:61661/heroku_app35867326',
+  PROD_DB_ADDRESS: 'mongodb://user:pass@appnum.mongolab.com:port/heroku_app_id_num',
   TEMPLATE_DIR: 'partials'
 };
